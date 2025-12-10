3ccc1fc4066baccdaa77f5db945e3c67fccb0b6c
Sushil-Tamang2022 <sushiltamang411@gmail.com>
2025-12-10 23:48:53 +0545

    Added Conditional_Statements

---
+++ conditionalStatement.js
@@ -0,0 +1,75 @@
+// Conditional Statement
+// if-else
+    // if(true) {
+    //     //statement
+    //     console.log("if part")
+    // } else {
+    //     console.log("else part")
+    // }
+
+    let x = 100;
+    let y = 100;
+
+    // if (x < y) {
+    //     console.log("if")
+    // } else {
+    //     console.log("else")
+    // }
+
+    // if-else-lader
+    // if (x > y) {
+    //     console.log("x")
+    // } else if (x < y) {
+    //     console.log("y")
+    // } else if (x == y) {
+    //     console.log("They are equal in value")
+    // } else {
+    //     console.log("else")
+    // }
+
+
+// let age = 0
+// if (age == 0) {
+//     console.log("You are not born.")
+// } else if (age > 0 && age <= 5) {
+//     console.log("Child")
+// } else if (age > 5 && age <= 12) {
+//     console.log("mid child")
+// } else if (age >= 13 && age < 20) {
+//     console.log("Teen")
+// } else if (age >= 20 && age <=30) {
+//     console.log("Young")
+// } else {
+//     console.log("Old")
+// }
+
+
+// Grading system
+let marks = 80;
+
+
+if (marks >= 90 && marks <= 100) {
+    console.log("A+")
+} else if (marks >= 80 && marks < 90) {
+    console.log("A-")
+} else if (marks >=70 && marks < 80) {
+    console.log("A")
+} else if (marks >= 60 && marks < 70) {
+    console.log("B+")
+} else if (marks >= 50 && marks < 60) {
+    console.log("B-")
+} else if (marks >= 40 && marks < 50) {
+    console.log("B")
+} else if (marks >= 30 && marks < 40) {
+    console.log("C+")
+} else if (marks >= 20 && marks < 30) {
+    console.log("C")
+} else if (marks >= 10 && marks <20) {
+    console.log("D")
+} else if (marks >= 0 && marks < 19) {
+    console.log("Not a Grade")
+}
+
+
+
+    
