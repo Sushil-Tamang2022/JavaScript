f68b72dbee3cdd58d617c0613e9190754f36fb7e
Sushil-Tamang2022 <sushiltamang411@gmail.com>
2025-12-10 23:46:35 +0545

    Adding operators

---
+++ operators.js
@@ -0,0 +1,103 @@
+// Operators
+// 1. Arithmetic Operators
+// -, +, /, *, %, **, --, ++
+console.log(9+1)
+console.log(9-1)
+console.log(9*1)
+console.log(9/1)
+console.log(9%1)
+console.log(9**2)
+
+// const { isValidElement } = require("react")
+
+let x = 10
+console.log(x++) // post increment
+console.log(x--) // post decrement
+console.log(++x) // pre increment
+console.log(x--) // pre decrement
+console.log(243243%2) it helps to check ODD & Even
+
+
+// 2. Assignment Operators
+// =, -=, +=, *=, /=, %=, **=
+// let x = 100
+// x+=5 //x+5
+// x-=5 //x-5
+// x/=5 //x/5
+// x*=5 //x*5
+// x%=5 //x%5
+// x**=5 //x**5
+// console.log(x)
+
+
+// 3. Comparision Operators
+// ==, ===, >=, <=, >, <, !=, !==
+let s = 100
+let y = 100
+console.log(x == y) // it checks value
+
+let a = 100
+let b = "100"
+console.log(x === y) // it checks value and data type
+
+// let x = 100
+// let y = 200
+// console.log(x > y)
+// console.log(x < y) 
+// console.log(x >= y) // x is greater than Y or x is equal to y
+// console.log(x <= y) // x is less than y or is equal to y
+
+// let x = 10
+// let y = 10
+// // console.log(x != y)
+// console.log(x !== y)
+
+
+
+// 4. Logical Operators
+// &&, ||, ! There should have 2 conditions
+// cond1 && cond2
+// true && true = true
+// false && false = false
+// false && true = false
+// true && false = false
+
+// or ||
+// true || true = true
+// true || false = true
+// false || true = true
+// false || false = false
+
+let d = 10
+let f = 12
+console.log(x > y || x <= y)
+console.log(x >= y || x <= y)
+console.log(x < y || x <= y)
+
+// not operator (!)
+let isAmin = true
+console.log(!isAmin)
+console.log(isAmin)
+
+
+// 5. Ternary Operators
+// ? : it is alternative of if-else
+true ? console.log("Hi") : console.log("Bye")
+false ? console.log("Hi") : console.log("Bye")
+
+
+let age = 10
+let fullName = `I am ${age} years old`
+console.log(fullName)
+
+// let x = 10
+// let y = 20
+
+// let sum = x + y
+// console.log(`Result = ${sum}`)
+
+
+
+
+
+
